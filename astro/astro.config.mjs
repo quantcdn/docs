372c90e41d6051812344729af60fb79924c9fbd4
Stuart Rowlands <stuart.rowlands@quantcdn.io>
2024-07-22 20:01:35 +1000

    Added site for sitemap.

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://docs.quantcdn.io',
 	integrations: [
 		starlight({
 			title: 'QuantCDN',
